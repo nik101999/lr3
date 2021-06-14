ff42cbd4774fd363d0cb4fc09dd904a6df781dc9
nik101999 <85872177+nik101999@users.noreply.github.com>
2021-06-14 18:20:30 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,10 +1,3 @@
-from flask import request
-from flask import Response
-import base64
-from PIL import Image
-from io import BytesIO
-import json
-
 print("Hello world")
 from flask import Flask
 app = Flask(__name__)
@@ -14,8 +7,7 @@ def hello():
  return " <html><head></head> <body> Hello World! </body></html>"
 if __name__ == "__main__":
  app.run(host='127.0.0.1',port=5000)
- 
- from flask import render_template
+from flask import render_template
 #наша новая функция сайта
 @app.route("/data_to")
 def data_to():
@@ -25,7 +17,7 @@ def data_to():
  some_value = 10
  #передаем данные в шаблон и вызываем его
  return render_template('simple.html',some_str = some_str,
- some_value = some_value,some_pars=some_pars)
+                        some_value = some_value,some_pars=some_pars) 
 
 # модули работы с формами и полями в формах
 from flask_wtf import FlaskForm,RecaptchaField
@@ -39,7 +31,8 @@ app.config['SECRET_KEY'] = SECRET_KEY
 # используем капчу и полученные секретные ключи с сайта google 
 app.config['RECAPTCHA_USE_SSL'] = False
 app.config['RECAPTCHA_PUBLIC_KEY'] = '6LeMTzEbAAAAAOVvop7U91SZcrnjgO03XRwXK3Sr'
-app.config['RECAPTCHA_PRIVATE_KEY'] = '6LeMTzEbAAAAAIXuQ-3CRSyM6jK0668PzXW4qKkV'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LeMTzEbAAAAAIXuQ-3CRSyM6jK0668PzXW4qKkV
+'
 app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
 # обязательно добавить для работы со стандартными шаблонами
 from flask_bootstrap import Bootstrap
@@ -78,53 +71,77 @@ def net():
  neurodic = {}
  # проверяем нажатие сабмит и валидацию введенных данных
  if form.validate_on_submit():
- # файлы с изображениями читаются из каталога static
- filename = os.path.join('./static', secure_filename(form.upload.data.filename))
- fcount, fimage = neuronet.read_image_files(10,'./static')
- # передаем все изображения в каталоге на классификацию
- # можете изменить немного код и передать только загруженный файл
- decode = neuronet.getresult(fimage)
- # записываем в словарь данные классификации
- for elem in decode:
- neurodic[elem[0][1]] = elem[0][2]
- # сохраняем загруженный файл
- form.upload.data.save(filename)
+  # файлы с изображениями читаются из каталога static
+  filename = os.path.join('./static', secure_filename(form.upload.data.filename))
+  fcount, fimage = neuronet.read_image_files(10,'./static')
+  # передаем все изображения в каталоге на классификацию
+  # можете изменить немного код и передать только загруженный файл
+  decode = neuronet.getresult(fimage)
+  # записываем в словарь данные классификации
+  for elem in decode:
+   neurodic[elem[0][1]] = elem[0][2]
+  # сохраняем загруженный файл
+  form.upload.data.save(filename)
  # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
  # сети если был нажат сабмит, либо передадим falsy значения
  return render_template('net.html',form=form,image_name=filename,neurodic=neurodic)
-bootstrap = Bootstrap(app)
 
+from flask import request
+from flask import Response
+import base64
+from PIL import Image
+from io import BytesIO
+import json
 # метод для обработки запроса от пользователя
 @app.route("/apinet",methods=['GET', 'POST'])
 def apinet():
+ print("1")
  neurodic = {}
  # проверяем что в запросе json данные
  if request.mimetype == 'application/json': 
- # получаем json данные
- data = request.get_json()
- # берем содержимое по ключу, где хранится файл
- # закодированный строкой base64
- # декодируем строку в массив байт, используя кодировку utf-8
- # первые 128 байт ascii и utf-8 совпадают, потому можно
- filebytes = data['imagebin'].encode('utf-8')
- # декодируем массив байт base64 в исходный файл изображение
- cfile = base64.b64decode(filebytes)
- # чтобы считать изображение как файл из памяти используем BytesIO
- img = Image.open(BytesIO(cfile))
- decode = neuronet.getresult([img])
- neurodic = {}
- for elem in decode:
- neurodic[elem[0][1]] = str(elem[0][2])
- print(elem)
- # пример сохранения переданного файла
- # handle = open('./static/f.png','wb')
- # handle.write(cfile)
- # handle.close()
+  # получаем json данные
+  print(request.__dir__())
+  data = request.get_json()
+  # берем содержимое по ключу, где хранится файл
+  # закодированный строкой base64
+  # декодируем строку в массив байт, используя кодировку utf-8
+  # первые 128 байт ascii и utf-8 совпадают, потому можно
+  print(data)
+  filebytes = data['imagebin'].encode('utf-8')
+  # декодируем массив байт base64 в исходный файл изображение
+  cfile = base64.b64decode(filebytes)
+  print("4")
+  # чтобы считать изображение как файл из памяти используем BytesIO
+  img = Image.open(BytesIO(cfile))
+  decode = neuronet.getresult([img])
+  neurodic = {}
+  for elem in decode:
+   neurodic[elem[0][1]] = str(elem[0][2])
+   print(elem)
+  # пример сохранения переданного файла
+  # handle = open('./static/f.png','wb')
+  # handle.write(cfile)
+  # handle.close()
  # преобразуем словарь в json строку
  ret = json.dumps(neurodic)
  # готовим ответ пользователю
  resp = Response(response=ret,
- status=200,
- mimetype="application/json")
+                 status=200,
+                 mimetype="application/json")
  # возвращаем ответ
- return resp
+ return resp 
+
+import lxml.etree as ET
+@app.route("/apixml",methods=['GET', 'POST'])
+def apixml():
+ #парсим xml файл в dom
+ dom = ET.parse("./static/xml/file.xml")
+ #парсим шаблон в dom
+ xslt = ET.parse("./static/xml/file.xslt")
+ #получаем трансформер
+ transform = ET.XSLT(xslt)
+ #преобразуем xml с помощью трансформера xslt
+ newhtml = transform(dom)
+ #преобразуем из памяти dom в строку, возможно, понадобится указать кодировку
+ strfile = ET.tostring(newhtml)
+ return strfile
